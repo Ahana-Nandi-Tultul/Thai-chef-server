999574141bf83fc7ff754e489d2a85390a2dbfe1
Ahana-Nandi-Tultul <83442291+Ahana-Nandi-Tultul@users.noreply.github.com>
2023-08-13 16:48:36 +0600

    fetch chef_id wise recipes

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const chefs = require('./data/chefs.json');
-const recipes = require('./data/recipes.json');
+const thai_foods = require('./data/recipes.json');
+const recipes = thai_foods.thai_foods;
 
 const cors = require('cors');
 app.use(cors());
@@ -16,7 +17,12 @@ app.get('/chefs', (req, res) => {
     res.send(chefs);
 })
 
-
+app.get('/recipes/:chefId', (req, res) => {
+    const chefId = parseInt(req.params.chefId);
+    console.log(chefId);
+    const selectedRecipes = recipes.filter(re => parseInt(re.chef_id) === chefId);
+    res.send(selectedRecipes);
+})
 
 app.listen(port, () => {
     console.log(`Orchid is running on port: ${port}`);
