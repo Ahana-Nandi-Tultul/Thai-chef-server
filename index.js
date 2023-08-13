1c6ab99ecfc355329553563848f9ab242a5bae16
Ahana-Nandi-Tultul <83442291+Ahana-Nandi-Tultul@users.noreply.github.com>
2023-08-13 16:23:16 +0600

    chefs data is added

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const chefs = require('./data/chefs.json');
 
 const cors = require('cors');
 app.use(cors());
+
 
 app.get('/', (req, res) => {
     res.send('Thai Orchid Bristo is comming');
 })
 
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
+})
+
 app.listen(port, () => {
-    console.log('Orchid is running on port:', $port);
+    console.log(`Orchid is running on port: ${port}`);
 })
